025929de5410dc7ebdff14859ce45fb1444935bd
Oleg <oleg.smirnov@exactprosystems.com>
2023-08-17 18:13:37 +0400

    Add tests for custom fields

--- src/test/kotlin/com/exactpro/th2/dataprocessor/zephyr/impl/scale/TestZephyrScaleEventProcessorImpl.kt
+++ src/test/kotlin/com/exactpro/th2/dataprocessor/zephyr/impl/scale/TestZephyrScaleEventProcessorImpl.kt
@@ -20,7 +20,12 @@ import com.exactpro.th2.common.event.EventUtils
 import com.exactpro.th2.common.grpc.EventID
 import com.exactpro.th2.common.grpc.EventStatus
 import com.exactpro.th2.dataprocessor.zephyr.cfg.ConnectionCfg
+import com.exactpro.th2.dataprocessor.zephyr.cfg.ConstantValue
 import com.exactpro.th2.dataprocessor.zephyr.cfg.EventProcessorCfg
+import com.exactpro.th2.dataprocessor.zephyr.cfg.EventValue
+import com.exactpro.th2.dataprocessor.zephyr.cfg.EventValueKey
+import com.exactpro.th2.dataprocessor.zephyr.cfg.JiraValue
+import com.exactpro.th2.dataprocessor.zephyr.cfg.JiraValueKey
 import com.exactpro.th2.dataprocessor.zephyr.service.api.JiraApiService
 import com.exactpro.th2.dataprocessor.zephyr.service.api.model.AccountInfo
 import com.exactpro.th2.dataprocessor.zephyr.service.api.model.Issue
@@ -74,7 +79,12 @@ internal class TestZephyrScaleEventProcessorImpl {
     private val processor = ZephyrScaleEventProcessorImpl(
         configuration = EventProcessorCfg(
             "TEST_T\\d+",
-            statusMapping = statusMapping
+            statusMapping = statusMapping,
+            customFields = mapOf(
+                "Const" to ConstantValue(42),
+                "Jira" to JiraValue(JiraValueKey.VERSION),
+                "Event" to EventValue(EventValueKey.NAME),
+            )
         ),
         mapOf(ConnectionCfg.DEFAULT_NAME to ScaleServiceHolder(jira, zephyr)), dataProvider,
     )
@@ -153,7 +163,11 @@ internal class TestZephyrScaleEventProcessorImpl {
                     argThat { name == statusMapping[testCaseStatus] },
                     argThat { contains(testCase.eventId.id) },
                     same(accountInfo),
-                    eq(emptyMap()),
+                    eq(mapOf(
+                        "Const" to 42,
+                        "Jira" to versionValue,
+                        "Event" to testCase.eventName,
+                    )),
                 )
                 verifyNoMoreInteractions()
             }
@@ -232,7 +246,11 @@ internal class TestZephyrScaleEventProcessorImpl {
                     argThat { name == "PASS" },
                     argThat { contains(testCase.eventId.id) },
                     same(accountInfo),
-                    eq(emptyMap()),
+                    eq(mapOf(
+                        "Const" to 42,
+                        "Jira" to version.name,
+                        "Event" to testCase.eventName,
+                    )),
                 )
                 verifyNoMoreInteractions()
             }
@@ -253,7 +271,11 @@ internal class TestZephyrScaleEventProcessorImpl {
                     argThat { name == "PASS" },
                     argThat { contains(testCase.eventId.id) },
                     same(accountInfo),
-                    eq(emptyMap()),
+                    eq(mapOf(
+                        "Const" to 42,
+                        "Jira" to version.name,
+                        "Event" to testCase.eventName,
+                    )),
                 )
                 verifyNoMoreInteractions()
             }
